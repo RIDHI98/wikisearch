3e04d513227947c78a4c064285bf07b2882b52f2
RIDHI98 <33255681+RIDHI98@users.noreply.github.com>
2021-02-05 19:35:27 +0530

    Add files via upload

---
+++ wikisearchbash.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+if [[ -z "$1" ]]; then
+  echo "Please enter search word"
+  read word
+elif [[ -n "$1" ]]; then
+  word="$*"
+fi
+
+word="${word// /_}"
+
+echo "...Searchig for $word..."
+
+url="https://en.wikipedia.org/wiki/$word"
+
+echo "$link"
+destdir=log.txt
+echo "${link}" >> "$destdir"
